e341aef5dab4159c8b5174a085c900f30ab025ce
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-10-21 15:03:38 +0300

    Update rubocop offenses

--- db/migrate/20221021113708_add_user_ref_to_posts.rb
+++ db/migrate/20221021113708_add_user_ref_to_posts.rb
@@ -1,7 +1,5 @@
-# rubocop:disable Layout/EndOfLine
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
     add_reference :posts, :user, null: false, foreign_key: true
   end
 end
-# rubocop:enable Layout/EndOfLine
--- db/migrate/20221021113847_add_user_and_post_ref_to_comments.rb
+++ db/migrate/20221021113847_add_user_and_post_ref_to_comments.rb
@@ -1,8 +1,6 @@
-# rubocop:disable Layout/EndOfLine
 class AddUserAndPostRefToComments < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :user, null: false, foreign_key: true
     add_reference :comments, :post, null: false, foreign_key: true
   end
 end
-# rubocop:enable Layout/EndOfLine
