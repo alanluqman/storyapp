f1b9aea1c3b3ff5d286db093bb8b004c508d7dc3
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-10-21 14:55:05 +0300

    Add processing data in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :likes
+  has_many :comments
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- db/migrate/20221020182834_create_likes.rb
+++ db/migrate/20221020182834_create_likes.rb
@@ -1,10 +1,5 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
-      t.references :post, null: false, foreign_key: true
-      t.references :author, null: false, foreign_key: { to_table: :users }
-
-      t.timestamps
-    end
+    create_table :likes, &:timestamps
   end
 end
