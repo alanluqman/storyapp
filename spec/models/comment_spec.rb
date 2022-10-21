2a237d22b61c66d5b5e46a9a5ecfbb3cc065a338
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-10-21 20:29:41 +0300

    Fix rubocop offenses

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   subject do
-    user = User.new(name: 'Alan Luqman', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from UK.', post_counter: 0)
+    user = User.new(name: 'Alan Luqman', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from UK.',
+                    post_counter: 0)
     post = Post.new(author: user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
-    Comment.new(post: post, author: user, text: 'Hi Alan!')
+    Comment.new(post:, author: user, text: 'Hi Alan!')
   end
 
   it '# is valid with valid attributes' do
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-   subject do
-    user = User.new(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 0)
+  subject do
+    user = User.new(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                    post_counter: 0)
     post = Post.new(user_id: user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
-    Like.new(author: user, post: post)
+    Like.new(author: user, post:)
   end
 
   it 'is valid with valid attributes' do
